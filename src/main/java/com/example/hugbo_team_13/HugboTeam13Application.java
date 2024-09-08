346a6214150531f9c435b1755f6b8d87a4c4a42a
dannith <dannitorr@gmail.com>
2024-09-08 16:03:02 +0000

    init commit Daniel

--- src/main/java/com/example/hugbo_team_13/HugboTeam13Application.java
+++ src/main/java/com/example/hugbo_team_13/HugboTeam13Application.java
@@ -3,6 +3,7 @@ package com.example.hugbo_team_13;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Daniel var her
 @SpringBootApplication
 public class HugboTeam13Application {
 
