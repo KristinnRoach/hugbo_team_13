cdc9d126366e7c84546d76271e00b44760dd61ed
Kristinn Roach Gunnarsson <kristinnroachgunnarsson@gro-dw030.rhi.hi.is>
2024-09-26 20:20:14 +0000

    Bætti við H2 local in memory testDb. Testaði endapunkta: Create User (POST), Delete user (DELETE), update user (PUT),  get all users (GET) og get user by id (GET). Allt virkar so far :)

---
+++ src/main/java/com/example/hugbo_team_13/controller/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.hugbo_team_13.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAllExceptions(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
